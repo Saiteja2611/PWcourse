444dfb4dec6f87b1ece2bb4393c86dd26722a951
Saiteja2611 <147418786+Saiteja2611@users.noreply.github.com>
2023-12-18 21:25:13 +0530

    Add files via upload

---
+++ input and operators.cpp
@@ -0,0 +1,41 @@
+1 
+output:--> 0 
+
+
+2 
+output:--> 1  1  
+
+
+3  
+output:--> 2  
+
+
+4  
+#include<iostream>
+using namespace std;
+int main(){
+    int radius,height;
+    cin>>radius;
+    cin>>height;
+    float pi=3.14;
+    int x=pi*radius*radius*height;
+    cout<<"Volume of cylinder:"<< x ;
+    
+} 
+
+
+5
+#include<iostream>
+using namespace std;
+int main(){
+    char ch1,ch2;
+    cin>>ch1;
+    cin>>ch2;
+    cout<<ch2-ch1;
+}
+
+6 
+output:---> 49
+
+
+
