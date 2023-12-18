e20d06cc72323441140f078355e0de06740e1da6
Saiteja2611 <147418786+Saiteja2611@users.noreply.github.com>
2023-12-18 21:11:24 +0530

    Add files via upload

---
+++ output and variables.cpp
@@ -0,0 +1,54 @@
+1  
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Physics"<<endl;
+    cout<<"Wallah";
+}
+
+
+
+2  
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int b=5;
+    cout<<a+b;
+} 
+
+
+
+3  
+int---->4 
+bool--->1 
+float--->4 
+
+Total ----> 9   
+
+
+4  
+output:---> 6  4 
+
+
+
+5 
+#include <iostream>
+using namespace std;
+int main(){
+    int radius=10;
+    float pi=3.1415;
+    float area=2*pi*radius;
+    cout<<area;
+}
+
+
+
+6
+_FLOAT and FLOAT--> 2
+
+
+
